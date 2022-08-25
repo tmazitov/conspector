6117275b609eb8821767588812f2e35e5534d78b
Timur Mazitov <timurmazitov000@gmail.com>
2022-08-25 13:49:24 +0300

    change port of aaa

--- cmd/aaa/aaa.go
+++ cmd/aaa/aaa.go
@@ -30,5 +30,5 @@ func main() {
 
 	aaa.SetupService(r, db, redis, conf)
 
-	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
